b5f9618ff3e13fcdced28bc10839c88317892381
Tibor Répási <rtib@users.noreply.github.com>
2019-07-10 22:16:21 +0200

    fix spec

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'cassandra::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:pre_condition) { 'include cassandra' }
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
