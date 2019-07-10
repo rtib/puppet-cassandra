e8263b701737ee14ee445bf8c8ea9013e076e9de
Tibor Répási <rtib@users.noreply.github.com>
2019-07-10 22:10:36 +0200

    fix missing context

--- spec/acceptance/30_cluster_spec.rb
+++ spec/acceptance/30_cluster_spec.rb
@@ -1,23 +1,25 @@
 require 'spec_helper_acceptance'
 require 'rspec/retry'
 
-describe port(9042) do
-  it 'port 9042 is eventually listening', retry: 15, retry_wait: 5 do
-    is_expected.to be_listening
+context 'service tests' do
+  describe port(9042) do
+    it 'port 9042 is eventually listening', retry: 15, retry_wait: 5 do
+      is_expected.to be_listening
+    end
   end
-end
 
-describe 'nodetool info', retry: 15, retry_wait: 5 do
-  subject(:nodetool_info) { run_shell('nodetool info') }
+  describe 'nodetool info', retry: 15, retry_wait: 5 do
+    subject(:nodetool_info) { run_shell('nodetool info') }
 
-  its(:exit_code) { is_expected.to eq 0 }
-  its(:stdout) { is_expected.to match %r{Gossip active *: true} }
-  its(:stdout) { is_expected.to match %r{Native Transport active *: true} }
-end
+    its(:exit_code) { is_expected.to eq 0 }
+    its(:stdout) { is_expected.to match %r{Gossip active *: true} }
+    its(:stdout) { is_expected.to match %r{Native Transport active *: true} }
+  end
 
-describe 'nodetool describecluster', retry: 15, retry_wait: 5 do
-  subject(:nodetool_describecluster) { run_shell('nodetool describecluster') }
+  describe 'nodetool describecluster', retry: 15, retry_wait: 5 do
+    subject(:nodetool_describecluster) { run_shell('nodetool describecluster') }
 
-  its(:exit_code) { is_expected.to eq 0 }
-  its(:stdout) { is_expected.to match %r{Name: Test cluster} }
+    its(:exit_code) { is_expected.to eq 0 }
+    its(:stdout) { is_expected.to match %r{Name: Test cluster} }
+  end
 end
