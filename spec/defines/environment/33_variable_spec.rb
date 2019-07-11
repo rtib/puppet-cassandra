75fe7f53fc07b157ec326191f7a103c1c2eb028f
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 15:29:59 +0200

    variable spec

--- spec/defines/environment/33_variable_spec.rb
+++ spec/defines/environment/33_variable_spec.rb
@@ -13,7 +13,13 @@ describe 'cassandra::environment::variable' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      it { is_expected.to compile.with_all_deps }
+      it do
+        is_expected.to contain_concat__fragment('cassandra::environment::variable[testvariable]=testvalue')
+          .with_target('/var/lib/cassandra/.cassandra.in.sh')
+          .with_order('10')
+          .with_content(%r{^testvariable=\"testvalue\"$})
+      end
     end
   end
 end
