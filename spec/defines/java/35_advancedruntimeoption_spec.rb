26a48cfa4192fa0b6365b903345040d793a0092e
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 22:41:35 +0200

    advancedruntimeoption spec

--- spec/defines/java/35_advancedruntimeoption_spec.rb
+++ spec/defines/java/35_advancedruntimeoption_spec.rb
@@ -2,18 +2,52 @@ require 'spec_helper'
 
 describe 'cassandra::java::advancedruntimeoption' do
   let(:pre_condition) { 'include cassandra' }
-  let(:title) { 'testtitle' }
-  let(:params) do
-    {
-      'value' => 'testvalue',
-    }
-  end
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      context 'option switch on' do
+        let(:title) { 'OptiOn' }
+        let(:params) do
+          {
+            'value' => true,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('XX:+OptiOn')
+        end
+      end
+
+      context 'option switch off' do
+        let(:title) { 'OptiOff' }
+        let(:params) do
+          {
+            'value' => false,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('XX:-OptiOff')
+        end
+      end
+
+      context 'option parameter' do
+        let(:title) { 'Option' }
+        let(:params) do
+          {
+            'value' => 'Parameter',
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('XX:Option=Parameter')
+        end
+      end
     end
   end
 end
