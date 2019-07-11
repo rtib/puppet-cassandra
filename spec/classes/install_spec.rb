474d97e44433b872b8f2446017912a79b7d021cf
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 11:59:01 +0200

    firm spec for install
    improved wording

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -5,12 +5,51 @@ describe 'cassandra' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) { os_facts }
-        let(:pre_condition) { 'include cassandra' }
 
-        it { is_expected.to compile }
-        it do
-          is_expected.to contain_package('cassandra')
-          is_expected.to contain_package('cassandra-tools')
+        context 'default install' do
+          let(:pre_condition) { 'include cassandra' }
+
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_package('cassandra')
+              .with_ensure('installed')
+          end
+          it do
+            is_expected.to contain_package('cassandra-tools')
+              .with_ensure('installed')
+          end
+        end
+        context 'specific version install' do
+          let(:params) do
+            {
+              'cassandra_ensure' => '3.0.18',
+            }
+          end
+
+          it do
+            is_expected.to contain_package('cassandra')
+              .with_ensure('3.0.18')
+          end
+          it do
+            is_expected.to contain_package('cassandra-tools')
+              .with_ensure('3.0.18')
+          end
+        end
+        context 'different tools and cassandra version install' do
+          let(:params) do
+            {
+              'tools_ensure' => '3.0.18',
+            }
+          end
+
+          it do
+            is_expected.to contain_package('cassandra')
+              .with_ensure('installed')
+          end
+          it do
+                is_expected.to contain_package('cassandra-tools')
+              .with_ensure('3.0.18')
+          end
         end
       end
     end
