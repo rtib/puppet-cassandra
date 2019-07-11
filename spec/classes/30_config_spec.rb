3f48d8656660a33ff63e25f35169ebb4efee9803
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 12:58:01 +0200

    config spec

--- spec/classes/30_config_spec.rb
+++ spec/classes/30_config_spec.rb
@@ -1,12 +1,58 @@
 require 'spec_helper'
 
-describe 'cassandra::config' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:pre_condition) { 'include cassandra' }
-      let(:facts) { os_facts }
+describe 'cassandra' do
+  describe 'cassandra::config' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+        context 'default setup' do
+          it { is_expected.to compile.with_all_deps }
+          it do
+            is_expected.to contain_concat('/var/lib/cassandra/.cassandra.in.sh')
+              .with_ensure('present')
+              .with_ensure_newline(true)
+          end
+          it do
+            is_expected.to contain_concat__fragment('cassandra.in.sh header')
+              .with_target('/var/lib/cassandra/.cassandra.in.sh')
+              .with_order('01')
+              .with_source('puppet:///modules/cassandra/cassandra.in.sh.header')
+          end
+          it { is_expected.to contain_class('cassandra::config::topology') }
+          it { is_expected.to contain_class('cassandra::config::rackdc') }
+          it do
+            is_expected.to contain_yaml_settings('cassandra::config')
+              .with_target('/etc/cassandra/cassandra.yaml')
+          end
+        end
+        context 'alternate path setup' do
+          let(:params) do
+            {
+              'cassandra_home' => '/opt/cassandra',
+              'config_dir'     => '/etc/cassandra/conf-2',
+            }
+          end
+
+          it do
+            is_expected.to contain_concat('/opt/cassandra/.cassandra.in.sh')
+              .with_ensure('present')
+              .with_ensure_newline(true)
+          end
+          it do
+            is_expected.to contain_concat__fragment('cassandra.in.sh header')
+              .with_target('/opt/cassandra/.cassandra.in.sh')
+              .with_order('01')
+              .with_source('puppet:///modules/cassandra/cassandra.in.sh.header')
+          end
+          it { is_expected.to contain_class('cassandra::config::topology') }
+          it { is_expected.to contain_class('cassandra::config::rackdc') }
+          it do
+            is_expected.to contain_yaml_settings('cassandra::config')
+              .with_target('/etc/cassandra/conf-2/cassandra.yaml')
+          end
+        end
+      end
     end
   end
 end
