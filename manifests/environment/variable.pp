ad43b960e3944980138c6235a45c1be46cea50f1
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 15:29:50 +0200

    simplify resource name

--- manifests/environment/variable.pp
+++ manifests/environment/variable.pp
@@ -7,7 +7,7 @@
 define cassandra::environment::variable (
   String $value
 ) {
-  concat::fragment{ "cassandra::environment::variable[${name}]=${value}@${facts['hostname']}":
+  concat::fragment{ "cassandra::environment::variable[${name}]=${value}":
     target   => $cassandra::envfile,
     order    => '10',
     content  => inline_epp('<%= $name %>="<%= $val -%>"',
