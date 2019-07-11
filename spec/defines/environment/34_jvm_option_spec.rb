f97edddb3a16b00e72f0f8a1b76bdb6db1aec9d8
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 17:34:16 +0200

    jvm_option spec

--- spec/defines/environment/34_jvm_option_spec.rb
+++ spec/defines/environment/34_jvm_option_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 describe 'cassandra::environment::jvm_option' do
   let(:pre_condition) { 'include cassandra' }
-  let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
+  let(:title) { '-verbose:gc' }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it do
+        is_expected.to contain_cassandra__environment__variable('JVM_EXTRA_OPTS')
+          .with_value('$JVM_EXTRA_OPTS -verbose:gc')
+      end
     end
   end
 end
