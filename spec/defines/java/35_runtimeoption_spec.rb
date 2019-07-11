7fcc7b23be16322964ac2f0cec19d9754a9fb6b6
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 22:52:06 +0200

    runtimeoption spec

--- spec/defines/java/35_runtimeoption_spec.rb
+++ spec/defines/java/35_runtimeoption_spec.rb
@@ -2,16 +2,33 @@ require 'spec_helper'
 
 describe 'cassandra::java::runtimeoption' do
   let(:pre_condition) { 'include cassandra' }
-  let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      context 'simple option' do
+        let(:title) { 'testoption' }
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('Xtestoption')
+        end
+      end
+
+      context 'option /w parameter' do
+        let(:title) { 'testoption2' }
+        let(:params) do
+          {
+            'value' => 'testvalue'
+          }
+        end
+
+        it { is_expected.to compile }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('Xtestoption2=testvalue')
+        end
+      end
     end
   end
 end
