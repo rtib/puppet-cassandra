16762d804e745eded4b470ffda990a61a3cf8a7d
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 13:44:04 +0200

    service spec

--- spec/classes/40_service_spec.rb
+++ spec/classes/40_service_spec.rb
@@ -1,12 +1,35 @@
 require 'spec_helper'
 
-describe 'cassandra::service' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:pre_condition) { 'include cassandra' }
-      let(:facts) { os_facts }
+describe 'cassandra' do
+  describe 'cassandra::service' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+        context 'default setup' do
+          it { is_expected.to compile.with_all_deps }
+          it do
+            is_expected.to contain_service('cassandra')
+              .without_ensure
+              .with_enable('manual')
+          end
+        end
+        context 'service enabled and running setup' do
+          let(:params) do
+            {
+              'service_ensure' => 'running',
+              'service_enable' => true,
+            }
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it do
+            is_expected.to contain_service('cassandra')
+              .with_ensure('running')
+              .with_enable(true)
+          end
+        end
+      end
     end
   end
 end
