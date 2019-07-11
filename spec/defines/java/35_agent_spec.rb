a40d998182a5d0e7e8f130f3cc63dcf33a305087
Tibor Répási <rtib@users.noreply.github.com>
2019-07-11 22:32:39 +0200

    agent spec

--- spec/defines/java/35_agent_spec.rb
+++ spec/defines/java/35_agent_spec.rb
@@ -2,16 +2,33 @@ require 'spec_helper'
 
 describe 'cassandra::java::agent' do
   let(:pre_condition) { 'include cassandra' }
-  let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      context 'simple agent' do
+        let(:title) { 'testAgent1' }
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('javaagent:testAgent1')
+        end
+      end
+
+      context 'agent /w param' do
+        let(:title) { 'testAgent2' }
+        let(:params) do
+          {
+            'value' => 'param'
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_cassandra__environment__jvm_option('javaagent:testAgent2=param')
+        end
+      end
     end
   end
 end
