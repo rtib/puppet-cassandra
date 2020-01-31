f4a0cdd4bcede8a2564f5a67112a2d7758f7ab65
Tibor Répási <rtib@users.noreply.github.com>
2020-01-31 12:30:30 +0100

    upgrade to pdk 1.15.0

---
+++ spec/spec_helper_acceptance_local.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rspec/retry'
+include PuppetLitmus
+
+bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures/cassandra.sources.list'), '/etc/apt/sources.list.d/cassandra.sources.list')
+run_shell('apt-get update')
