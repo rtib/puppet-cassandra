421179861242edac8c8693e334b01818236ffacf
Tibor Répási <rtib@users.noreply.github.com>
2020-01-31 18:09:05 +0100

    prepare 4.0 package repo

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -3,5 +3,5 @@
 require 'rspec/retry'
 include PuppetLitmus
 
-bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures/cassandra.sources.list'), '/etc/apt/sources.list.d/cassandra.sources.list')
+bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures/cassandra_311x.sources.list'), '/etc/apt/sources.list.d/cassandra.sources.list')
 run_shell('apt-get update')
