6f943baa5802d5a85d3ec36f4a86b8a6eaeaa643
Tibor Répási <rtib@users.noreply.github.com>
2020-02-03 16:43:51 +0100

    test targets v4 defined

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -3,5 +3,9 @@
 require 'rspec/retry'
 include PuppetLitmus
 
-bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures/cassandra_311x.sources.list'), '/etc/apt/sources.list.d/cassandra.sources.list')
-run_shell('apt-get update')
+node_vars = vars_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST'])
+
+if node_vars.key?('apt_sources_list')
+  bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', node_vars['apt_sources_list']), '/etc/apt/sources.list.d/cassandra.sources.list')
+  run_shell('apt-get update')
+end
