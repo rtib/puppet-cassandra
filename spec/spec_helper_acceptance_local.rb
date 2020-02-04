7500ba52685db271d6fbb3bd4a53af76cc41c12c
Tibor Répási <rtib@users.noreply.github.com>
2020-02-04 16:45:46 +0100

    move back to ssh docker provisioner
    provisioner testing
    
    
    testing with travis
    
    
    bug

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -3,7 +3,7 @@
 require 'rspec/retry'
 include PuppetLitmus
 
-node_vars = vars_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST'])
+node_vars = vars_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST']) || {}
 
 if node_vars.key?('apt_sources_list')
   bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', node_vars['apt_sources_list']), '/etc/apt/sources.list.d/cassandra.sources.list')
