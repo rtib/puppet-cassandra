8f89733a7817bd1ef201d335f9f723ed2fa4057f
Tibor Répási <rtib@users.noreply.github.com>
2020-07-13 16:43:44 +0200

    New feature: JVM option sets (#11)

---
+++ manifests/jvm_option_set.pp
@@ -0,0 +1,101 @@
+# @summary Set JVM options by controlling particular lines of jvm.options file.
+#
+# Select the file to be controlled by choosing, jvm, jvm8 or jvm11 and the
+# variant server or clients. Options, properties and advanced runtime options
+# can be defined to have particular values or to be removed from the configuration.
+# Any option not mentioned will not be touched.
+#
+# For Cassandra 3.x versions, only `optsfile = jvm` with `variant = undef` is supported,
+# which will control the `/etc/cassandra/jvm.options` file. Since Cassandra versions >= 4.0
+# use distinct option files for server and clients, as well as Java independent, Java-8 and
+# Java-11, use parameters `optsfile` and `variant` to select a particular options file.
+#
+# @param optsfile
+#   determine the file to control, either `jvm` for the independet
+#   options or `jvm8` or `jvm11` for the version dependant options
+# @param variant
+#   leave this undef for Cassandra < 4.0, set it to `server` or `clients` if running >= 4.0
+# @param options
+#   list of basic JVM options, e.g. `ea`, `server`, `Xms4g`, etc.
+# @param properties
+#   java properties to be passed to the JVM
+# @param advancedoptions
+#   advanced runtime options which may be feature toggles or values
+define cassandra::jvm_option_set (
+  Enum['jvm', 'jvm8', 'jvm11']        $optsfile = 'jvm',
+  Optional[Enum['clients', 'server']] $variant = undef,
+  Array[String]                       $options = [],
+  Hash[String,Optional[Scalar]]       $properties = {},
+  Hash[String,Optional[Scalar]]       $advancedoptions = {},
+) {
+  $_file = $variant ? {
+    /^(server|clients)$/ => "${cassandra::config_dir}/${optsfile}-${variant}.options",
+    default              => "${cassandra::config_dir}/${optsfile}.options",
+  }
+
+  $options.each |String $_opt| {
+    if $_opt =~ /^~/ {
+      $_value = $_opt[1,-1]
+      file_line { "${name} remove option ${_value}":
+        ensure => absent,
+        path   => $_file,
+        line   => "-${_value}",
+      }
+    } else {
+      file_line { "${name} set option ${_opt}":
+        ensure => present,
+        path   => $_file,
+        line   => "-${_opt}",
+      }
+    }
+  } # $options.each
+
+  $properties.each |String $_prop, Data $_value| {
+    if $_value =~ Undef {
+      file_line { "${name} remove property ${_prop}":
+        ensure            => absent,
+        path              => $_file,
+        match             => "^-D${_prop}",
+        match_for_absence => true,
+      }
+    } else {
+      file_line { "${name} set property ${_prop}":
+        ensure => present,
+        path   => $_file,
+        match  => "^-D${_prop}",
+        line   => "-D${_prop}=${_value}",
+      }
+    }
+  } # $properties.each
+
+  $advancedoptions.each |String $_aopt, Data $_value| {
+    if $_value =~ Undef {
+      file_line { "${name} remove advanced runtime option ${_aopt}":
+        ensure            => absent,
+        path              => $_file,
+        match             => "^-XX:[+-]?${_aopt}",
+        match_for_absence => true,
+      }
+    } else {
+      if $_value =~ Boolean {
+        $_pre = $_value ? {
+          true    => '+',
+          default => '-',
+        }
+        file_line { "${name} set boolean advanced runtime option ${_aopt}":
+          ensure => present,
+          path   => $_file,
+          match  => "^-XX:[+-]${_aopt}",
+          line   => "-XX:${_pre}${_aopt}",
+        }
+      } else {
+        file_line { "${name} set advanced runtime option ${_aopt}":
+          ensure => present,
+          path   => $_file,
+          match  => "^-XX:${_aopt}",
+          line   => "-XX:${_aopt}=${_value}",
+        }
+      }
+    }
+  }
+}
---
+++ spec/defines/35_jvm_option_set_spec.rb
@@ -0,0 +1,160 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'cassandra::jvm_option_set' do
+  let(:pre_condition) { 'include cassandra' }
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'Cassandra 4 Java-8 server options' do
+        let(:title) { 'C*4 Java-8 server options' }
+        let(:params) do
+          {
+            'optsfile'   => 'jvm8',
+            'variant'    => 'server',
+            'advancedoptions' => {
+              'ThreadPriorityPolicy' => 42,
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('C*4 Java-8 server options set advanced runtime option ThreadPriorityPolicy')
+            .with_ensure('present')
+            .with_path('/etc/cassandra/jvm8-server.options')
+            .with_match('^-XX:ThreadPriorityPolicy')
+            .with_line('-XX:ThreadPriorityPolicy=42')
+        end
+      end
+      context 'Cassandra 4 Java-11 server options' do
+        let(:title) { 'C*4 Java-11 server options' }
+        let(:params) do
+          {
+            'optsfile'   => 'jvm11',
+            'variant'    => 'server',
+            'advancedoptions' => {
+              'ThreadPriorityPolicy' => 42,
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('C*4 Java-11 server options set advanced runtime option ThreadPriorityPolicy')
+            .with_ensure('present')
+            .with_path('/etc/cassandra/jvm11-server.options')
+            .with_match('^-XX:ThreadPriorityPolicy')
+            .with_line('-XX:ThreadPriorityPolicy=42')
+        end
+      end
+      context 'Cassandra 4 indep server options' do
+        let(:title) { 'C*4 indep server options' }
+        let(:params) do
+          {
+            'variant'    => 'server',
+            'properties' => {
+              'cassandra.available_processors' => 4,
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('C*4 indep server options set property cassandra.available_processors')
+            .with_ensure('present')
+            .with_path('/etc/cassandra/jvm-server.options')
+            .with_match('^-Dcassandra.available_processors')
+            .with_line('-Dcassandra.available_processors=4')
+        end
+      end
+      context 'add/remove options' do
+        let(:title) { 'a/r opts' }
+        let(:params) do
+          {
+            'options' => ['server', '~ea'],
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('a/r opts remove option ea')
+            .with_ensure('absent')
+            .with_line('-ea')
+        end
+        it do
+          is_expected.to contain_file_line('a/r opts set option server')
+            .with_ensure('present')
+            .with_line('-server')
+        end
+      end
+      context 'add/remove properties' do
+        let(:title) { 'a/r props' }
+        let(:params) do
+          {
+            'properties' => {
+              'test1' => :undef,
+              'test2' => 'bla',
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('a/r props remove property test1')
+            .with_ensure('absent')
+            .with_match('^-Dtest1')
+            .with_match_for_absence(true)
+        end
+        it do
+          is_expected.to contain_file_line('a/r props set property test2')
+            .with_ensure('present')
+            .with_line('-Dtest2=bla')
+            .with_match('^-Dtest2')
+        end
+      end
+      context 'add/remove advanced options' do
+        let(:title) { 'a/r adv opts' }
+        let(:params) do
+          {
+            'advancedoptions' => {
+              'test1' => :undef,
+              'test2' => true,
+              'test3' => false,
+              'test4' => 'bla',
+            },
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file_line('a/r adv opts remove advanced runtime option test1')
+            .with_ensure('absent')
+            .with_match('^-XX:[+-]?test1')
+            .with_match_for_absence(true)
+        end
+        it do
+          is_expected.to contain_file_line('a/r adv opts set boolean advanced runtime option test2')
+            .with_ensure('present')
+            .with_line('-XX:+test2')
+            .with_match('^-XX:[+-]test2')
+        end
+        it do
+          is_expected.to contain_file_line('a/r adv opts set boolean advanced runtime option test3')
+            .with_ensure('present')
+            .with_line('-XX:-test3')
+            .with_match('^-XX:[+-]test3')
+        end
+        it do
+          is_expected.to contain_file_line('a/r adv opts set advanced runtime option test4')
+            .with_ensure('present')
+            .with_line('-XX:test4=bla')
+            .with_match('^-XX:test4')
+        end
+      end
+    end
+  end
+end
