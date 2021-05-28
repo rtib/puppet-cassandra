004ca9e7ce79f86e5e8dba6a0509e4f0517ad2dd
Tibor Répási <rtib@users.noreply.github.com>
2021-05-28 16:35:13 +0200

    testing extended dynamic matrix

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -9,3 +9,8 @@ if node_vars.key?('apt_sources_list')
   bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', node_vars['apt_sources_list']), '/etc/apt/sources.list.d/cassandra.sources.list')
   run_shell('apt-get update')
 end
+
+if node_vars.key?('release')
+  bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', "cassandra_#{node_vars['release']}.sources.list"), '/etc/apt/sources.list.d/cassandra.sources.list')
+  run_shell('apt-get update')
+end
