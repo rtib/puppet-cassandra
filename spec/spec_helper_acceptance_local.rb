a12fb9e6f9eba31392f330ebf82525def8ed12b0
Tibor Répási <rtib@users.noreply.github.com>
2021-05-28 16:57:22 +0200

    dynamic test matrix, update os support (#30)

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -5,7 +5,7 @@ include PuppetLitmus
 
 node_vars = vars_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST']) || {}
 
-if node_vars.key?('apt_sources_list')
-  bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', node_vars['apt_sources_list']), '/etc/apt/sources.list.d/cassandra.sources.list')
+if node_vars.key?('release')
+  bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', "cassandra_#{node_vars['release']}.sources.list"), '/etc/apt/sources.list.d/cassandra.sources.list')
   run_shell('apt-get update')
 end
