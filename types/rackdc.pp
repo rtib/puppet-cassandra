e0d6995514fbf2291a2f5768668a8dddad52412e
Tibor Répási <rtib@users.noreply.github.com>
2024-10-18 13:54:19 +0200

    fix validation issues

--- types/rackdc.pp
+++ types/rackdc.pp
@@ -2,8 +2,8 @@
 # Note, that the fields `dc` and `rack` mandatory to setup rackdc, while
 # `dc_suffix` and `prefer_local` can be set optionally.
 type Cassandra::Rackdc = Struct[{
-  dc           => String,
-  rack         => String,
-  dc_suffix    => Optional[String],
-  prefer_local => Optional[Boolean],
+    dc           => String,
+    rack         => String,
+    dc_suffix    => Optional[String],
+    prefer_local => Optional[Boolean],
 }]
